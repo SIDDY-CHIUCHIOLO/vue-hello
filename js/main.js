cabc21c2f187ddfe5682c81b3bc7b563dcc0b274
siddhartha chiuchiolo <siddychiuchiolo7@gmail.com>
2022-03-16 15:36:17 +0100

    inserito messaggio a schermo all'interno di un h1

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+const root = new Vue({
+        el: "#root",
+        data: {
+            message: "ciao",
+        }
+    }
+);
